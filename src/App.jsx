afd3c109bf3155da886253b8b9e2df801113dfe7
noahspott <noahspottdev@gmail.com>
2024-10-29 10:37:06 -0400

    - complete configuration

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createRoot } from "react-dom";
+import Pet from "./Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
